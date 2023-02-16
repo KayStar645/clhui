ab9bc0b60f4dfc1bd1f78756846bc164f8c52b5f
thuan <thuanpt182@gmail.com>
2023-02-17 00:41:04 +0700

    check

--- run.java
+++ run.java
@@ -4,13 +4,17 @@ public class run {
     public static void main(String[] args) throws IOException {
         AlgoCLHMiner clhui = new AlgoCLHMiner();
 
-        int minUtil = 80000;
-        String inputPath = "./data/LiquorTransaction.txt";
-        String outputPath = "./data/LiquorOutput.txt";
-        String TaxonomyPath = "./data/LiquorTaxonomy.txt";
+        // int minUtil = 80000;
+        // String inputPath = "./data/LiquorTransaction.txt";
+        // String outputPath = "./data/LiquorOutput.txt";
+        // String TaxonomyPath = "./data/LiquorTaxonomy.txt";
+
+        int minUtil = 60;
+        String inputPath = "./test/input.txt";
+        String outputPath = "./test/output.txt";
+        String TaxonomyPath = "./test/taxonomy.txt";
 
         clhui.runAlgorithm(minUtil, inputPath, outputPath, TaxonomyPath);
         clhui.printStats();
-        // This is comment
     }
 }
